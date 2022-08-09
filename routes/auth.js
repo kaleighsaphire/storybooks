7badc51f4dde6f67444460384e8261c07ab2fafc
kaleighsaphire <kaleigh.scoffield@gmail.com>
2022-08-09 11:48:19 -0700

    nav and route protection

--- routes/auth.js
+++ routes/auth.js
@@ -8,8 +8,24 @@ router.get('/google', passport.authenticate('google', { scope: ['profile'] }))
 
 // @desc    Google auth callback
 // @route   GET /auth/google/callback
-router.get('/google/callback', passport.authenticate('google', {failureRedirect: '/'}), (req, res) => {
+router.get(
+    '/google/callback', 
+    passport.authenticate('google', 
+    {failureRedirect: '/'}), 
+    (req, res) => {
     res.redirect('/dashboard')
+    }
+)
+
+//@desc Logout User
+//@route /auth/logout
+
+// logout must be async
+router.get('/logout', (req, res, next) => {
+    req.logout(function(err) {
+        if (err) {return next(err)}
+        res.redirect('/')
+    })
 })
 
 module.exports = router
