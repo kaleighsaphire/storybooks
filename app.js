a947e4f6ee8858b0a720d556ed28528f813a2059
kaleighsaphire <kaleigh.scoffield@gmail.com>
2022-08-13 11:58:43 -0700

    add to stories form

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const morgan = require('morgan')
 const exphbs = require('express-handlebars')
 const passport = require('passport')
 const session = require('express-session')
+const MongoStore = require('connect-mongo')//(session) not neccessary 
 const connectDB = require('./config/db')
 
 // Load config
@@ -36,10 +37,13 @@ app.use(
         secret: 'keyboard cat',
         resave: false,
         saveUninitialized: false,
+        store: MongoStore.create({
+            mongoUrl: process.env.MONGO_URI
+        })
     })
 )
 
-// Passportt middleware
+// Passport middleware
 app.use(passport.initialize())
 app.use(passport.session())
 
@@ -49,6 +53,7 @@ app.use(express.static(path.join(__dirname, 'public')))
 // Routes 
 app.use('/', require('./routes/index'))
 app.use('/auth', require('./routes/auth'))
+app.use('/stories', require('./routes/stories'))
 
 const PORT = process.env.PORT || 8000
 
